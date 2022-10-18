51c25c35e4730d83ecd607a9722638ab59c9d56b
Alex <classic.alex.91@gmail.com>
2022-10-18 17:59:38 +0300

    add new programm lession 012

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-const category = 'toys';
+let personalMovieDB = {
+    count:  numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-console.log(`https://someurl.com/${category}/5`);
+let name_film1 = prompt('Один из последних просмотренных фильмов?', "");
+let points1 = prompt('На сколько оцените его?', "");
+let name_film2 = prompt('Один из последних просмотренных фильмов?', "");
+let points2 = prompt('На сколько оцените его?', "");
 
-const user = "Ivan";
+personalMovieDB.movies[name_film1] = points1;
+personalMovieDB.movies[name_film2] = points2;
 
-alert(`Hello, ${user}`);
-
+console.log(personalMovieDB);
