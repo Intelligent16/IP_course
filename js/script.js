a6c2d08ba26a587a62d475eea153624a68e9d3db
Alex <classic.alex.91@gmail.com>
2022-11-03 21:09:00 +0300

    lesson 15: for and if

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
-    count:  numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
+if (personalMovieDB.count < 10) {
+  console.log("Почему так мало, ты что, Саня Гиндосов?");
+} else if (10 <= personalMovieDB.count < 30) {
+  console.log("Ну, в принципе, нормально");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Ты, вообще, нормальный??");
+} else {
+  console.log("Давай нормально вписывай");
+}
 
-let name_film1 = prompt('Один из последних просмотренных фильмов?', "");
-let points1 = prompt('На сколько оцените его?', "");
-let name_film2 = prompt('Один из последних просмотренных фильмов?', "");
-let points2 = prompt('На сколько оцените его?', "");
+for (let i = 0; i < numberOfFilms; i++) {
+  let name_film = prompt("Один из последних просмотренных фильмов?", "");
+  let points = prompt("На сколько оцените его?", "");
+  if (
+    name_film != "" &&
+    name_film != null &&
+    name_film.length < 50 &&
+    points != "" &&
+    points != null
+  ) {
+    console.log("Спасибо");
+  } else {
+    console.log("Постарайтесь лучше");
+    i--;
+  }
 
-personalMovieDB.movies[name_film1] = points1;
-personalMovieDB.movies[name_film2] = points2;
+  personalMovieDB.movies[name_film] = points;
+}
 
 console.log(personalMovieDB);
