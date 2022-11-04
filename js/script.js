c8482249db7427c309db228847706abdb63d8a7c
Alex <classic.alex.91@gmail.com>
2022-11-04 23:27:23 +0300

    add functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -7,33 +7,48 @@ let personalMovieDB = {
   genres: [],
   privat: false,
 };
-if (personalMovieDB.count < 10) {
-  console.log("Почему так мало, ты что, Саня Гиндосов?");
-} else if (10 <= personalMovieDB.count < 30) {
-  console.log("Ну, в принципе, нормально");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Ты, вообще, нормальный??");
-} else {
-  console.log("Давай нормально вписывай");
-}
 
-for (let i = 0; i < numberOfFilms; i++) {
-  let name_film = prompt("Один из последних просмотренных фильмов?", "");
-  let points = prompt("На сколько оцените его?", "");
-  if (
-    name_film != "" &&
-    name_film != null &&
-    name_film.length < 50 &&
-    points != "" &&
-    points != null
-  ) {
-    console.log("Спасибо");
-  } else {
-    console.log("Постарайтесь лучше");
-    i--;
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
   }
-
-  personalMovieDB.movies[name_film] = points;
 }
+start();
+
+function rememberMyFilms() {
+  for (let i = 0; i < numberOfFilms; i++) {
+    let name_film = prompt("Один из последних просмотренных фильмов?", "");
+    let points = prompt("На сколько оцените его?", "");
+    if (
+      name_film != "" &&
+      name_film != null &&
+      name_film.length < 50 &&
+      points != "" &&
+      points != null
+    ) {
+      personalMovieDB.movies[name_film] = points;
+      console.log("Спасибо");
+    } else {
+      console.log("Постарайтесь лучше");
+      i--;
+    }
+  }
+}
+rememberMyFilms();
+
+function detectPersonalLvl() {
+  if (personalMovieDB.count < 10) {
+    console.log("Почему так мало, ты что, Саня Гиндосов?");
+  } else if (10 <= personalMovieDB.count < 30) {
+    console.log("Ну, в принципе, нормально");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Ты, вообще, нормальный??");
+  } else {
+    console.log("Давай нормально вписывай");
+  }
+}
+
+detectPersonalLvl();
 
 console.log(personalMovieDB);
