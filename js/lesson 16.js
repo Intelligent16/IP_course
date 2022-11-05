0a83f4d71af5b1ebe3e4853ee77633e160a5b1ca
Alex <classic.alex.91@gmail.com>
2022-11-05 22:13:52 +0300

    destruct

--- js/lesson 16.js
+++ js/lesson 16.js
@@ -61,3 +61,60 @@ let num = 0;
 // callback("Джава", text);
 
 // LESSON 20: OBJECTS
+
+let options = {
+  name: "test_name",
+  width: 123,
+  height: 123,
+  colors: {
+    border: "test_border",
+    backGround: "test_color",
+  },
+  makeTest: function () {
+    // добавляем функцию как свойство объекта, чтобы объект
+    //мог что-либо выполнять
+    console.log("test_function_on_object");
+  },
+};
+options.makeTest();
+
+// delete options.colors.backGround;
+// console.log(options.colors);
+
+// let count = 0; // для подсчета количества свойств
+// for (let key in options) {
+//   // перебираем объект
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//       count++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     count++;
+//   }
+// }
+//console.log(count);
+
+console.log(Object.keys(options)); // перебирает объект
+console.log(Object.keys(options).length); // колличество свойств объекта
+
+// Деструктуризация объекта - вынесение свойств за пределы объекта
+let destr = {
+  name: "test_name",
+  width: 123,
+  height: 123,
+  colors: {
+    border: "test_border",
+    backGround: "test_color",
+  },
+  makeTest: function () {
+    console.log("test_function_on_object");
+  },
+};
+
+let { width, name } = destr;
+console.log(width, name);
+
+let { border, backGround } = destr.colors;
+console.log(border, backGround);
