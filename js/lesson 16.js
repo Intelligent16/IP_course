eb5ba09b8e04da157766fa2f27379deb506c9d37
Alex <classic.alex.91@gmail.com>
2022-11-13 12:02:58 +0300

    add metods of personalMovieBD

--- js/lesson 16.js
+++ js/lesson 16.js
@@ -62,21 +62,26 @@ let num = 0;
 
 // LESSON 20: OBJECTS
 
-let options = {
-  name: "test_name",
-  width: 123,
-  height: 123,
-  colors: {
-    border: "test_border",
-    backGround: "test_color",
-  },
-  makeTest: function () {
-    // добавляем функцию как свойство объекта, чтобы объект
-    //мог что-либо выполнять
-    console.log("test_function_on_object");
-  },
-};
-options.makeTest();
+// let options = {
+//   name: "test_name",
+//   width: 123,
+//   height: 123,
+//   colors: {
+//     border: "test_border",
+//     backGround: "test_color",
+//   },
+//   makeTest: function () {
+//     // добавляем функцию как свойство объекта, чтобы объект
+//     //мог что-либо выполнять
+//     console.log("test_function_on_object");
+//   },
+// };
+// console.log(options["colors"]["border"]);
+// console.log(options.colors.border);
+// console.log(options.width);
+// options.makeTest();
+// options.width = 1024;
+// console.log(options);
 
 // delete options.colors.backGround;
 // console.log(options.colors);
@@ -94,27 +99,133 @@ options.makeTest();
 //     count++;
 //   }
 // }
-//console.log(count);
+// console.log(count);
 
-console.log(Object.keys(options)); // перебирает объект
-console.log(Object.keys(options).length); // колличество свойств объекта
+// console.log(Object.keys(options)); // перебирает объект
+// console.log(Object.keys(options).length); // колличество свойств объекта
 
-// Деструктуризация объекта - вынесение свойств за пределы объекта
-let destr = {
-  name: "test_name",
-  width: 123,
-  height: 123,
-  colors: {
-    border: "test_border",
-    backGround: "test_color",
-  },
-  makeTest: function () {
-    console.log("test_function_on_object");
-  },
+// // Деструктуризация объекта - вынесение свойств за пределы объекта
+// let destr = {
+//   name: "test_name",
+//   width: 123,
+//   height: 123,
+//   colors: {
+//     border: "test_border",
+//     backGround: "test_color",
+//   },
+//   makeTest: function () {
+//     console.log("test_function_on_object");
+//   },
+// };
+
+// let { width, name } = destr;
+// console.log(width, name);
+
+// let { border, backGround } = destr.colors;
+// console.log(border, backGround);
+
+// LESSON 21: ARRAY
+
+// let array = [4, 8, 15, 16, 23, 42];
+
+array.pop(); // удаляет и возвращает последний элемент массива
+console.log(array);
+array.push(42); // добавляем в конец 1 элемент
+console.log(array);
+
+let array = [4, 8, 15, 16, 23, 42];
+
+for (let i = 0; i < array.length; i++) {
+  // перебор по индексам
+  console.log(array[i]);
+}
+
+for (let i of array) {
+  // of работает только с массиво-подобными сущностями, например: массив, строка, псевдомассив, мап, сет
+  // перебор по элементам
+  console.log(i);
+}
+
+let array = [4, 8, 15, 16, 23, 42];
+array.forEach(); // нельзя break и continue
+
+let str = "Любимую не отдают";
+let arr = str.split(" ");
+console.log(arr);
+console.log(arr.join("|"));
+
+// LESSON 22: Spread оператор
+// простейшие объекты переприсваиваются без ссылок (примитивные типы данных).
+// объекты, вложенные объекты, массивы, функции - изменяются ссылочно, поэтому оригиналы нужно копировать перед изменением
+
+// способы копирования объектов:
+// 1
+let mainObject = {
+  a: 1,
+  b: 2,
+  c: 3,
 };
+function copy(mainObject) {
+  let copyObject = {};
+  for (i in mainObject) {
+    copyObject[i] = mainObject[i];
+  }
+  return copyObject;
+}
+let copyObject1 = copy(mainObject);
+console.log(mainObject);
+copyObject1.b = 0;
+console.log(copyObject1);
 
-let { width, name } = destr;
-console.log(width, name);
+// 2
+mainObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+let secondObject = {
+  d: 4,
+  e: 5,
+};
+console.log(Object.assign(mainObject, secondObject));
+let copy2 = Object.assign({}, secondObject);
+copy2.d = 0;
+console.log(secondObject);
+console.log(copy2);
 
-let { border, backGround } = destr.colors;
-console.log(border, backGround);
+// 3
+let array1 = [1, 2, 3];
+let array2 = array1.slice();
+array2[2] = 99;
+console.log(array1);
+console.log(array2);
+
+// 4
+let item1 = ["one", "two", "three"],
+  item2 = ["1", "2", "3"],
+  item3 = [...item1, ...item2, "99"];
+console.log(item3);
+
+function spread(l, k, j) {
+  console.log(l);
+  console.log(k);
+  console.log(j);
+}
+arr = [1, 2, 3];
+spread(...arr);
+
+// 5
+
+mainObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+let newObject = { ...mainObject };
+console.log(newObject);
+newObject.a = 99;
+console.log(newObject);
+console.log(mainObject);
+
+let a = [] + false - null + true;
+console.log(a);
--- js/script.js
+++ js/script.js
@@ -1,76 +1,76 @@
-let numberOfFilms;
-
 let personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
-};
-
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
-  }
-}
-start();
-
-function rememberMyFilms() {
-  for (let i = 0; i < numberOfFilms; i++) {
-    let name_film = prompt("Один из последних просмотренных фильмов?", "");
-    let points = prompt("На сколько оцените его?", "");
-    if (
-      name_film != "" &&
-      name_film != null &&
-      name_film.length < 50 &&
-      points != "" &&
-      points != null
+  start: function () {
+    personalMovieDB.count = +prompt("Сколько фильмов ты посмотрел?", "");
+    while (
+      personalMovieDB.count == "" ||
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count)
     ) {
-      personalMovieDB.movies[name_film] = points;
-      console.log("Спасибо");
-    } else {
-      console.log("Постарайтесь лучше");
-      i--;
+      personalMovieDB.count = +prompt("Сколько фильмов ты посмотрел?", "");
     }
-  }
-}
-rememberMyFilms();
-
-function detectPersonalLvl() {
-  if (personalMovieDB.count < 10) {
-    console.log("Почему так мало, ты что, Саня Гиндосов?");
-  } else if (10 <= personalMovieDB.count < 30) {
-    console.log("Ну, в принципе, нормально");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Ты, вообще, нормальный??");
-  } else {
-    console.log("Давай нормально вписывай");
-  }
-}
-detectPersonalLvl();
-
-function showMyBD() {}
-if (personalMovieDB.privat != false) {
-  console.log(personalMovieDB);
-}
-showMyBD();
-
-function writeYouGeners() {
-  for (let i = 1; i <= 3; i++) {
-    let currentGener = prompt(
-      "Три любимых жанра в порядке убывания любимости",
-      ""
-    );
-    if (currentGener != null && currentGener != "") {
-      personalMovieDB.genres[i - 1] = currentGener;
-      console.log("Спасибо");
-    } else {
-      console.log("Впиши что-нибудь");
-      i--;
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < personalMovieDB.count; i++) {
+      let name_film = prompt("Один из последних просмотренных фильмов?", "");
+      let points = prompt("На сколько оцените его?", "");
+      if (
+        name_film != "" &&
+        name_film != null &&
+        name_film.length < 50 &&
+        points != "" &&
+        points != null
+      ) {
+        personalMovieDB.movies[name_film] = points;
+        console.log("Спасибо");
+      } else {
+        console.log("Постарайтесь лучше");
+        i--;
+      }
     }
-  }
-}
-writeYouGeners();
-
-console.log(personalMovieDB);
+  },
+  detectPersonalLvl: function () {
+    if (personalMovieDB.count < 10) {
+      console.log("Почему так мало, ты что, Саня Гиндосов?");
+    } else if (10 <= personalMovieDB.count < 30) {
+      console.log("Ну, в принципе, нормально");
+    } else if (personalMovieDB.count >= 30) {
+      console.log("Ты, вообще, нормальный??");
+    } else {
+      console.log("Давай нормально вписывай");
+    }
+  },
+  showMyBD: () => {
+    if (personalMovieDB.privat != false) {
+      console.log(personalMovieDB);
+    }
+  },
+  writeYouGeners: function () {
+    for (let i = 1; i <= 3; i++) {
+      let currentGener = prompt(
+        "Три любимых жанра в порядке убывания любимости"
+      );
+      if (currentGener != null && currentGener != "") {
+        personalMovieDB.genres[i - 1] = currentGener;
+        console.log("Спасибо");
+      } else {
+        console.log("Впиши что-нибудь");
+        i--;
+      }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Под номером ${i + 1} жанр ${item}`);
+    });
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.privat === false) {
+      personalMovieDB.privat = true;
+    } else {
+      personalMovieDB.privat = false;
+    }
+  },
+};
